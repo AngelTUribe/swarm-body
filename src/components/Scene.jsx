1643807b55373a160de87f12715e78f532533d4f
Angel Uribe <atu42461@uga.edu>
2026-05-13 16:54:11 -0400

    Bug fix

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import Swarm from './Swarm'; 
 import SprawlParticles from './SprawlParticles';
 import SpatialObjects from './SpatialObjects'; 
-import SpatialDrive from './SpatialDrive'; // <--- 1. Import the new game
+import SpatialDrive from './SpatialDrive'; 
 
 const Scene = ({ handsPositionRef }) => {
   return (
     <Canvas camera={{ position: [0, 0, 5], fov: 75 }}>
       <Swarm handsPositionRef={handsPositionRef} />
       <SprawlParticles handsPositionRef={handsPositionRef} />
-      <SpatialObjects handsPositionRef={handsPositionRef} />
       
-      {/* 2. Replace NeonBuilder with SpatialDrive */}
+      {/* THE FIX: Suspense catches the font loading so the Canvas doesn't crash! */}
+      <Suspense fallback={null}>
+        <SpatialObjects handsPositionRef={handsPositionRef} />
+      </Suspense>
+      
       <SpatialDrive handsPositionRef={handsPositionRef} />
     </Canvas>
   );
